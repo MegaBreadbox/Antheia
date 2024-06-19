93ae60a21d41e1e3a1c9fd26e8e85bd598b78ee0
Joshua <joshuagutierrez724@gmail.com>
2024-06-19 14:33:15 -0400

    Moved processWelcomeText to viewModel

--- app/src/main/java/com/example/antheia_plant_manager/screens/WelcomeScreen.kt
+++ app/src/main/java/com/example/antheia_plant_manager/screens/WelcomeScreen.kt
@@ -1,35 +1,52 @@
 package com.example.antheia_plant_manager.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.hilt.navigation.compose.hiltViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @Composable
 fun WelcomeScreen(
+    modifier: Modifier = Modifier,
     viewModel: WelcomeViewModel = hiltViewModel<WelcomeViewModel>()
 ) {
-    Column(){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = modifier
+            .fillMaxSize()
+
+    ){
         val welcome = "Welcome\nto\nAntheia"
-        var processWelcome by remember { mutableStateOf("") }
-        LaunchedEffect(key1 = processWelcome) {
-            if(welcome.length != processWelcome.length) {
+        val uiState = viewModel.uiState.collectAsState()
+        LaunchedEffect(key1 = uiState.value.processWelcomeText) {
+            if(welcome.length != uiState.value.processWelcomeText.length) {
                 delay(100L)
-                processWelcome += welcome[processWelcome.length]
+                viewModel.updateWelcomeText(welcome[uiState.value.processWelcomeText.length])
             }
         }
         Text(
-            text = processWelcome,
+            text = uiState.value.processWelcomeText,
+            textAlign = TextAlign.Center,
             style = MaterialTheme.typography.displayLarge
         )
     }
--- app/src/main/java/com/example/antheia_plant_manager/screens/WelcomeViewModel.kt
+++ app/src/main/java/com/example/antheia_plant_manager/screens/WelcomeViewModel.kt
@@ -1,12 +1,29 @@
 package com.example.antheia_plant_manager.screens
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class WelcomeViewModel @Inject constructor(
 
 ): ViewModel() {
+    private val _uiState = MutableStateFlow(WelcomeUiState())
+    val uiState = _uiState.asStateFlow()
+
+    fun updateWelcomeText(input: Char) {
+        _uiState.update {
+            it.copy(processWelcomeText = it.processWelcomeText + input)
+        }
+    }
+
 
 }
+
+data class WelcomeUiState(
+    val processWelcomeText: String = ""
+)
